5d47bfc9b3c3136e2023f85d076b1da04a629499
ERFAN POURNASIRI <erfanpournasiri71@gmail.com>
2024-02-17 22:45:12 +0330

    create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import "./App.css";
 
-export default function App() {
+import { Box, Text } from "@chakra-ui/react";
+import AddNewTodo from "./components/AddNewTodo";
+import TodosList from "./components/TodosList";
+import TodosChart from "./components/TodosChart";
+
+
+
+
+function App() {
+
+    
+    
   return (
-    <div>App</div>
+    <Box minH={"100vh"} >
+          <Text fontSize={'3xl'} borderBottom={"1px"}
+    borderColor={"gray.200"} >TODO APP</Text>
+        <Box display={"flex"}  gap={4} pt={10}>
+          <Box display={"flex"} flexDir={"column"} gap={4}flex={1} >
+            <AddNewTodo/>
+            <TodosList/>
+          </Box>
+          <Box display={"flex"} flex={1}>
+            <TodosChart/>
+          </Box>
+        </Box>
+    </Box>
   )
 }
+
+export default App;
---
+++ src/components/TodosChart.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function TodosChart() {
+  return (
+    <div>TodosChart</div>
+  )
+}
