b980ac9471d414a27a7462458cf02c85b649d984
ERFAN POURNASIRI <erfanpournasiri71@gmail.com>
2024-02-17 23:25:06 +0330

    create todos List

--- src/components/TodosList.jsx
+++ src/components/TodosList.jsx
@@ -1,7 +1,79 @@
-import React from 'react'
+import { Box, Button, Flex, Text,Checkbox } from "@chakra-ui/react"
 
-export default function TodosList() {
+import { useTodoContext, useTodosDispatch } from "../context/todosContext"
+
+function NoteList() {
+
+
+    const todos = useTodoContext()
+ 
+
+   
+
+    
   return (
-    <div>TodosList</div>
+    <div className="note-list">
+        {
+            todos?.map(todo=>{
+               return(
+                <TodoItem
+                key={todo.id}
+                todo={todo}
+                />
+               ) 
+            })
+        }
+    </div>
   )
 }
+
+export default NoteList
+
+
+
+
+export function TodoItem({todo}){
+    const dispatch = useTodosDispatch()
+   
+    return(
+       <Flex flexDir={"column"} gap={2} mt={4} shadow={"lg"}
+       border={"1px"} borderColor={"blue.200"} p={4}
+       borderRadius={10} bgColor={`${ todo.completed ? "gray.200" : "white"}`} > 
+       <Flex display={"flex"} w={"full"} justifyContent={"space-between"}>
+           <Box >
+            <Text
+            textDecoration={todo.completed ? "line-through" : "none"}
+            fontWeight={"bold"} fontSize={"xl"}>{todo.title}</Text>
+           </Box>
+           <Box display={"flex"} gap={2} alignItems={"center"}>
+           <Checkbox 
+              onChange={()=>dispatch({type:"completed", payload:todo.id})}
+              colorScheme='green'
+               isChecked={todo.completed}
+             >
+             </Checkbox>
+            <Button
+            onClick={()=>dispatch({type:"removeTodo", payload:todo.id})}
+            bgColor={"red.500"}
+            _hover={{bgColor:"red.600"}}
+            >Delete</Button>
+           </Box>
+       </Flex>
+       <Box
+       borderTop={"1px"} borderColor={"gray.200"}
+       display={"flex"} w={"full"} justifyContent={"center"}>
+        <Text>
+        {new Date(todo.createdAt).toLocaleDateString("en-US",{
+        year:"numeric",
+        month:"long",
+        day:"numeric"
+    })}
+        </Text>
+       </Box>
+       </Flex>
+    )
+}
+
+
+
+
